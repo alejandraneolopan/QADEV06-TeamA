9df494de1356431224a132157b830b26400392a0
ArielYanarico <argos.yo@gmail.com>
2016-03-29 14:42:05 -0400

    CRUD test cases for resources and locations finished (refactor pending)

--- tests/crud/crud_locationTest.js
+++ tests/crud/crud_locationTest.js
@@ -1,13 +1,13 @@
 /**
- * CRUD Resource Test
+ * CRUD Location Test
  */
 var expect = require('chai').expect;
 var request = require('../../lib/RequestManager/manager.js');
 var generator = require('../../utils/generator.js');
 var condition = require('../../lib/Conditions/condition.js');
-var resourceId;
+var locationId;
 
-describe('CRUD methods for API-Resources', function(){
+describe('CRUD methods for API-Locations', function(){
 
     this.timeout(5000);
     this.slow(4000);
@@ -19,21 +19,21 @@ describe('CRUD methods for API-Resources', function(){
     });
 
     beforeEach(function(done){
-        var randomResource = generator
-                            .generator_resource
-                            .generateResource();
+        var randomLocation = generator
+                            .generator_location
+                            .generateLocation();
                             
         condition
             .preCondition
-            .insertResource(randomResource, function(result){
-                resourceId = result._id;
+            .insertLocation(randomLocation, function(result){
+                locationId = result._id;
                 done();
             });
     });
 
     afterEach(function(done){
-        if (resourceId !== undefined) {
-            condition.removeResource( resourceId, function(){
+        if (locationId !== undefined) {
+            condition.removeLocation( locationId, function(){
                 console.log('-----------------------------------');
                 done();
             }); 
@@ -42,18 +42,18 @@ describe('CRUD methods for API-Resources', function(){
 
     var test ;
 
-    it('Resource POST', function(done){
-        var randomResource = generator
-                            .generator_resource
-                            .generateResource();
+    it('Location POST', function(done){
+        var randomLocation = generator
+                            .generator_location
+                            .generateLocation();
 
         request
-            .mres
-            .postResource(randomResource, function(err, res){
+            .mloc
+            .postLocation(randomLocation, function(err, res){
                 var actualResult = res.body;
                 condition
                     .assertion 
-                    .findResource(res.body._id, function(result){
+                    .findLocation(res.body._id, function(result){
                         expect(actualResult.customName).to.equal(result.customName);
                         expect(actualResult.name).to.equal(result.name);
                         expect(actualResult.fontIcon).to.equal(result.fontIcon);
@@ -62,14 +62,14 @@ describe('CRUD methods for API-Resources', function(){
         });
     });
 
-    it('Resource GET', function(done){
+    it('Location GET', function(done){
         request
-            .mres
-            .getResourceID(resourceId, function(err, res){ //REVISAR getResourceById AND print URI
+            .mloc
+            .getLocationsID(locationId, function(err, res){ //REVISAR getLocationById AND print URI
                 var actualResult = res.body;
                 condition
                     .assertion 
-                    .findResource(res.body._id, function(result){
+                    .findLocation(res.body._id, function(result){
                         expect(actualResult.customName).to.equal(result.customName);
                         expect(actualResult.name).to.equal(result.name);
                         expect(actualResult.fontIcon).to.equal(result.fontIcon);
@@ -78,27 +78,27 @@ describe('CRUD methods for API-Resources', function(){
         }); 
     });
 
-    it('Resource GET-ALL', function(done){
+    it('Location GET-ALL', function(done){
         request
-            .mres
-            .getResource(function(err, res){ //REVISAR getResources AND print URI
+            .mloc
+            .getLocations(function(err, res){ //REVISAR getLocations AND print URI
                 var actualResult = res.body.length;
                 condition
                     .assertion 
-                    .findAllResources(function(result){
+                    .findAllLocations(function(result){
                         expect(actualResult).to.equal(result.length);
                         done();
                     });
         }); 
     });
 
-    it('Resource DELETE',function(done){
+    it('Location DELETE',function(done){
          request
-            .mres
-            .delResource(resourceId, function(err,res){ //REVISAR getResourceById AND print URI
+            .mloc
+            .delLocation(locationId, function(err,res){ //REVISAR getLocationById AND print URI
                 condition
                     .assertion 
-                    .findResource(res.body._id, function(result){
+                    .findLocation(res.body._id, function(result){
                         expect(result).to.equal(undefined);
                         done();
                     });
