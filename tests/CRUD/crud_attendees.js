1bdfceee06fc4a1996c3395abd91b00bd4acf745
Alejandra Neolopan <alejandra.neolopan@fundacion-jala.org>
2016-03-31 19:36:32 -0400

    Improving the code according standards 2

---
+++ tests/CRUD/crud_attendees.js
@@ -0,0 +1,36 @@
+var expect = require('chai').expect;
+var request = require('../../lib/RequestManager/manager.js');
+var generator = require('../../utils/generator.js');
+var dbQuery = require('../../lib/Conditions/dbQuery.js');
+var config = require('../../config/config.json');
+
+describe("CRUD - Attendees Service", function(){
+
+    this.slow(config.timeSlow);
+    this.timeout(config.timeOut);
+    var serviceId = null;
+
+    before(function(done){
+        request.authentication.postLogin(function(err, res){
+            done();
+        });
+    });
+
+    beforeEach(function(done){
+        dbQuery.preCondition.findAllServices(function(res){
+            serviceId = res[0]._id;
+            done();
+        });
+    });
+
+    it('GET /services/{:serviceId}/attendees{?filter}/ returns a user from a specific email service', function(done){
+        var filter = 'ro';
+        request.services.getAttendeesByService(serviceId, filter, function(err, res){
+            var resourcesList = res.body;
+            console.log(res.body);
+            dbQuery.postCondition
+            done();
+        });
+    });
+
+});
